cdf2e4f127a609bb3d6b41ad9fbf50b96198c475
Steck98 <steczkiewicz1998@Gmail.com>
2021-09-20 20:28:28 +0200

    Project Ended

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var scorePlayer=0;
+var scoreAI=0;
+var result=document.querySelector('.result');
+var wynik = document.getElementById('wynik');
 var buttons=document.querySelectorAll(".btn");
 var playerMove;
 buttons.forEach((button) => {
@@ -13,7 +17,6 @@ buttons.forEach((button) => {
     
       var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'Kamień';
 } else if (randomNumber== '2'){
@@ -29,6 +32,8 @@ displayResult(playerMove, computerMove);
 
 
 
+
+
 // playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 // console.log('Wpisana odpowiedź to: ' + playerInput);
 // if (playerInput == '1') {
@@ -41,13 +46,21 @@ displayResult(playerMove, computerMove);
 
 
 function displayResult(playerMove, computerMove){
+  result.classList.remove('draw','lose','win')
     if ( playerMove == computerMove){
-        printMessage("Remis");
+        result.innerText="Remis";
+        result.classList.add('draw');
     }
     else if( (playerMove == 'Kamień' && computerMove == 'Papier') || (playerMove == 'Papier' && computerMove == 'Nożyce') || (playerMove == 'Nożyce' && computerMove == 'Kamień')){
-        printMessage("przegrałeś")}
-    else printMessage("Wygrałeś");
-    
+      result.innerText="Przegrałeś";
+      wynik.innerText = `Wynik Komputera: ${scoreAI++}
+      Twoj Wynik: ${scorePlayer}`;
+        result.classList.add('lose');}
+    else {
+      result.innerText="Wygrałeś";
+      result.classList.add('win');
+      wynik.innerText = `Wynik Komputera: ${scoreAI}
+      Twoj Wynik: ${scorePlayer++}`;}
 }
 
 
